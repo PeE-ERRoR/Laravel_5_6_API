539fbab11edc6455bc0bf3f8dd17d5c7700f1b6d
PeE-ERRoR <33443084+PeE-ERRoR@users.noreply.github.com>
2018-08-01 17:35:27 +0700

    require auth && controller

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // date th
+        // 
+        // $dt = Carbon::now('Asia/Bangkok');
+        return view('home');
+    }
+}
